8d539eba4110d3a217f6134076254a508ed944a3
Zohair-coder <zohair.ul.hasan@gmail.com>
2024-12-21 08:51:45 -0500

    fixed multiple additional info bug

--- convert_data.py
+++ convert_data.py
@@ -1,69 +1,86 @@
 import json
 
-with open("raw_data.json") as f:
-    data = json.load(f)
 
-nodes = []
-links = []
+def main():
+    with open("raw_data.json") as f:
+        data = json.load(f)
 
-for crn, course_data in data.items():
-    course_id = f"{course_data['subject_code']} {course_data['course_number']}"
+    nodes = []
+    links = []
 
-    node_ids = [node["id"] for node in nodes]
+    for crn, course_data in data.items():
+        course_id = f"{course_data['subject_code']} {course_data['course_number']}"
 
-    if course_id not in node_ids:
-        nodes.append(
-            {
-                "id": course_id,
-                "group": 1,
-                "additionalInfo": [
-                    "Course Title: " + course_data["course_title"],
-                ],
-            }
-        )
+        node_ids = [node["id"] for node in nodes]
 
-    if not course_data["instructors"]:
-        continue
+        if course_id not in node_ids:
+            nodes.append(
+                {
+                    "id": course_id,
+                    "group": 1,
+                    "additionalInfo": [
+                        "Course Title: " + course_data["course_title"],
+                    ],
+                }
+            )
 
-    for instructor in course_data["instructors"]:
-        links.append(
-            {
-                "source": course_id,
-                "target": instructor["name"],
-                "value": 1,
-                "id": "",
-            }
-        )
-
-        if instructor["name"] in node_ids:
+        if not course_data["instructors"]:
             continue
 
-        additionalInfo = []
-        if "rating" in instructor and instructor["rating"]:
-            additionalInfo.append(
-                f"Average Rating: {instructor['rating']['avgRating']}"
+        for instructor in course_data["instructors"]:
+            if not isLink(course_id, instructor["name"], links):
+                links.append(
+                    {
+                        "source": course_id,
+                        "target": instructor["name"],
+                        "value": 1,
+                        "id": "",
+                    }
+                )
+
+            if instructor["name"] in node_ids:
+                continue
+
+            additionalInfo = []
+            if "rating" in instructor and instructor["rating"]:
+                additionalInfo.append(
+                    f"Average Rating: {instructor['rating']['avgRating']}"
+                )
+
+                additionalInfo.append(
+                    f"Average Difficulty: {instructor['rating']['avgDifficulty']}"
+                )
+
+                additionalInfo.append(
+                    f"Number of ratings: {instructor['rating']['numRatings']}"
+                )
+
+                additionalInfo.append(
+                    f"<a href='https://www.ratemyprofessors.com/professor/{instructor['rating']['legacyId']}' target='_blank'>View ratings</a>"
+                )
+
+            nodes.append(
+                {
+                    "id": instructor["name"],
+                    "group": 2,
+                    "additionalInfo": additionalInfo,
+                }
             )
 
-            additionalInfo.append(
-                f"Average Difficulty: {instructor['rating']['avgDifficulty']}"
-            )
+    groupMapping = {1: "Course", 2: "Instructor"}
 
-            additionalInfo.append(
-                f"Number of ratings: {instructor['rating']['numRatings']}"
-            )
-
-            additionalInfo.append(
-                f"<a href='https://www.ratemyprofessors.com/professor/{instructor['rating']['legacyId']}' target='_blank'>View ratings</a>"
-            )
-
-        nodes.append(
-            {"id": instructor["name"], "group": 2, "additionalInfo": additionalInfo}
+    with open("course_data.json", "w") as f:
+        json.dump(
+            {"nodes": nodes, "links": links, "groupMapping": groupMapping}, f, indent=4
         )
 
 
-groupMapping = {1: "Course", 2: "Instructor"}
+def isLink(source, target, links):
+    for link in links:
+        if link["source"] == source and link["target"] == target:
+            return True
+    return False
 
-with open("course_data.json", "w") as f:
-    json.dump(
-        {"nodes": nodes, "links": links, "groupMapping": groupMapping}, f, indent=4
-    )
+
+if __name__ == "__main__":
+    main()
